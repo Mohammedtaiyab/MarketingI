0fcb9558e2139fc487acc7de82ac2579bbcb4eca
mohammedbori <mohammedbori44@gmail.com>
2020-07-27 15:15:18 +0530

    Contact

--- contact.php
+++ contact.php
@@ -1,3 +1,103 @@
+
+<?php
+if(isset($_POST["submit"])){
+$subject = $_POST['name'];
+$name=$_POST['name'];
+$email=$_POST['email'];
+$phone=$_POST['contact'];
+$campany=$_POST['company'];
+$msg=$_POST['message'];
+$message="Name :".$name."\n"."Phone :".$phone."\n"."Company :".$company."\n"."Wrote the following :"."\n\n".$msg;
+$emailaddress = "bforus.in@gmail.com";
+echo $message;
+$mail=mail($emailaddress, $subject, $message, "From:client@bforus.com");
+if ($mail){
+echo alert("Message has been sent");
+}
+else{
+echo"Message not sent this time";
+}
+echo"Message not sent this time";
+
+}*/
+?>
+<?php
+if(isset($_POST["submit"])){
+require "PHPMailer/PHPMailerAutoload.php";
+
+function smtpmailer($to, $from, $from_name, $subject, $body)
+    {
+    	$name=$_POST['name'];
+		$email=$_POST['email'];
+		$phone=$_POST['contact'];
+		$campany=$_POST['company'];
+		$msg=$_POST['message'];
+        $mail = new PHPMailer();
+        $mail->IsSMTP();
+        $mail->SMTPAuth = true; 
+ 
+        $mail->SMTPSecure = 'ssl'; 
+        $mail->Host = 'mail.marketingojo.com';
+        $mail->Port = 465;  
+        $mail->Username = 'info@marketingojo.com';
+        $mail->Password = 'Marketing@52';   
+   
+   //   $path = 'reseller.pdf';
+   //   $mail->AddAttachment($path);
+   
+        $mail->IsHTML(true);
+        $mail->From='info@marketingojo.com';
+     	 $mail->FromName=$from_name;
+        $mail->Sender=$from;
+        $mail->AddReplyTo($from, $from_name);
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+        $mail->AddAddress($to);
+        if(!$mail->Send())
+        {
+            header("Location: contact.php");
+            $error ="Please try Later, Error Occured while Processing...";
+            return $error; 
+        }
+        else 
+        {
+              header("Location: contact.php");
+            $error = "Thanks You !! Your email is sent.";  
+            return $error;
+        }
+    }
+    
+
+ 	$to   = 'info@marketingojo.com';
+    $from = $_POST['email'];
+    $name = $_POST['name'];
+    $subj = 'Inquiry';
+    $msg = "Name :".$name."<br>"."Phone :".$_POST['contact']."<br>"."Company :".$_POST['company']."<br>"."Services Required:<br>".implode(',', $_POST['service'])."<br>"."Wrote the following :"."<br>".$_POST['message'];
+  
+
+    $error=smtpmailer($to,$from, $name ,$subj, $msg);
+
+
+
+    $to   = $_POST['email'];
+    $from = 'info@marketingojo.com';
+    $name ='MarketingOJO';
+    $subj = 'Service/Product inquiry';
+    $msg = '<p>Thank you for your inquiry regarding our product and service.
+Your inquiry will be reviewed by the concerned team and will be getting in touch with you soon.
+Thanks again for your interest.<br>
+
+<span>Best Regards</span><br>
+<span>MarketingOJO Team</span><br>
+To know more about our services and products please visit: - 
+Instagram: - https://www.instagram.com/marketingojo/
+Facebook: - https://www.facebook.com/marketingojo/
+Website: - http://marketingojo.com/</p>';
+  
+
+    $error=smtpmailer($to,$from, $name ,$subj, $msg);
+}
+?>
 <!DOCTYPE html>
 <html class="no-js" lang="en">
     <head>
@@ -243,7 +343,7 @@ input[type=checkbox]{
 						<!-- Contact Form -->
 						  <center><h2 style="padding-top:70px;color: white;"><?php echo $error; ?></h2></center>
 						<div class="col-md-8 col-sm-6 col-xs-12">
-							<form class="form" method="post" action="mailhandler.php">
+							<form class="form" method="post" action="contact.php">
 								<div class="row">
 									<div class="col-md-6">
 										<div class="form-group">
