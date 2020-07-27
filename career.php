bde4cad35af93678b6db1d2f3e71afa8b9e41fda
mohammedbori <mohammedbori44@gmail.com>
2020-07-28 00:35:47 +0530

    Contact us

--- career.php
+++ career.php
@@ -21,7 +21,49 @@ function smtpmailer($to, $from, $from_name, $subject, $body)
    
 $path = 'upload/' . $_FILES["resume"]["name"];
  move_uploaded_file($_FILES["resume"]["tmp_name"], $path);
- 		
+ 		$email_message = '<h2>Contact Request Submitted</h2>
+                    <p><b>Name:</b> '.$name.'</p>
+                    <p><b>Email:</b> '.$email.'</p>
+                    <p><b>Subject:</b> '.$subject.'</p>
+                    <p><b>Message:</b><br/>'.$message.'</p>';
+$email_message.="Please find the attachment";
+$semi_rand = md5(uniqid(time()));
+$headers = "From: ".$fromemail;
+$mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
+ 
+    $headers .= "\nMIME-Version: 1.0\n" .
+    "Content-Type: multipart/mixed;\n" .
+    " boundary=\"{$mime_boundary}\"";
+ 
+if($_FILES["resume"]["name"]!= ""){  
+ $strFilesName = $_FILES["resume"]["name"];  
+ $strContent = chunk_split(base64_encode(file_get_contents($_FILES["resume"]["tmp_name"])));  
+ 
+ 
+    $email_message .= "This is a multi-part message in MIME format.\n\n" .
+    "--{$mime_boundary}\n" .
+    "Content-Type:text/html; charset=\"iso-8859-1\"\n" .
+    "Content-Transfer-Encoding: 7bit\n\n" .
+    $email_message .= "\n\n";
+ 
+ 
+    $email_message .= "--{$mime_boundary}\n" .
+    "Content-Type: application/octet-stream;\n" .
+    " name=\"{$strFilesName}\"\n" .
+    //"Content-Disposition: attachment;\n" .
+    //" filename=\"{$fileatt_name}\"\n" .
+    "Content-Transfer-Encoding: base64\n\n" .
+    $strContent  .= "\n\n" .
+    "--{$mime_boundary}--\n";
+}$toemail="info@marketingojo.com"; 
+ 
+if(mail($toemail, $subject, $email_message, $headers)){
+   $statusMsg= "Email send successfully with attachment";
+}else{
+   $statusMsg= "Not sent";
+}
+}
+   
  		
         $mail->IsHTML(true);
         $mail->From='info@marketingojo.com';
