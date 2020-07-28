c54a5185174e7089e055ea9f62d0ecca39d4ee93
mohammedbori <mohammedbori44@gmail.com>
2020-07-28 11:28:56 +0530

    career

--- career.php
+++ career.php
@@ -1,133 +1,74 @@
 
 <?php
-/*$postData = $uploadedFile = $statusMsg = '';
-$msgClass = 'errordiv';
-if(isset($_POST['submit'])){
-    // Get the submitted form data
-    $postData = $_POST;
-    $email = $_POST['email'];
-    $name = $_POST['name'];
-    $subject = $_POST['profile'];
-    $message = $_POST['bio'];
-    
-    // Check whether submitted data is not empty
-    if(!empty($email) && !empty($name) && !empty($subject) && !empty($message)){
-        
-        // Validate email
-        if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
-            $statusMsg = 'Please enter your valid email.';
-        }else{
-            $uploadStatus = 1;
-            
-            // Upload attachment file
-            if(!empty($_FILES["resume"]["name"])){
-                
-                // File path config
-                $targetDir = "uploads/";
-                $fileName = basename($_FILES["resume"]["name"]);
-                $targetFilePath = $targetDir . $fileName;
-                $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
-                
-                // Allow certain file formats
-                $allowTypes = array('pdf', 'doc', 'docx', 'jpg', 'png', 'jpeg');
-                if(in_array($fileType, $allowTypes)){
-                    // Upload file to the server
-                    if(move_uploaded_file($_FILES["resume"]["tmp_name"], $targetFilePath)){
-                        $uploadedFile = $targetFilePath;
-                    }else{
-                        $uploadStatus = 0;
-                        $statusMsg = "Sorry, there was an error uploading your file.";
-                    }
-                }else{
-                    $uploadStatus = 0;
-                    $statusMsg = 'Sorry, only PDF, DOC, JPG, JPEG, & PNG files are allowed to upload.';
-                }
-            }
-            
-            if($uploadStatus == 1){
-                
-                // Recipient
-                $toEmail = 'info@marketingojo.com';
+if(isset($_FILES['resume'])){
+      $errors= array();
+      $file_name = $_FILES['resume']['name'];
+      $file_size = $_FILES['resume']['size'];
+      $file_tmp = $_FILES['resume']['tmp_name'];
+      $file_type = $_FILES['resume']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['resume']['name'])));
+      
+      $expensions= array("jpeg","jpg","png","pdf");
+      
+      if(in_array($file_ext,$expensions)=== false){
+         $errors[]="extension not allowed, please choose a PDF, JPEG or PNG file.";
+      }
+      
+      if($file_size > 2097152) {
+         $errors[]='File size must be excately 2 MB';
+      }
+      
+      if(empty($errors)==true) {
+         move_uploaded_file($file_tmp,"uploads/".$file_name); //The folder where you would like your file to be saved
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
 
-                // Sender
-                $from = $_POST['email'];
-                $fromName = $_POST['name'];
-                
-                // Subject
-                $emailSubject = 'Contact Request Submitted by '.$name;
-                
-                // Message 
-                $htmlContent = '<h2>Contact Request Submitted</h2>
-                    <p><b>Name:</b> '.$name.'</p>
-                    <p><b>Email:</b> '.$email.'</p>
-                    <p><b>Subject:</b> '.$subject.'</p>
-                    <p><b>Message:</b><br/>'.$message.'</p>';
-                
-                // Header for sender info
-                $headers = "From: $fromName"." <".$from.">";
+// PHPMailer script below
 
-                if(!empty($uploadedFile) && file_exists($uploadedFile)){
-                    
-                    // Boundary 
-                    $semi_rand = md5(time()); 
-                    $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x"; 
-                    
-                    // Headers for attachment 
-                    $headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\""; 
-                    
-                    // Multipart boundary 
-                    $message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" .
-                    "Content-Transfer-Encoding: 7bit\n\n" . $htmlContent . "\n\n"; 
-                    
-                    // Preparing attachment
-                    if(is_file($uploadedFile)){
-                        $message .= "--{$mime_boundary}\n";
-                        $fp =    @fopen($uploadedFile,"rb");
-                        $data =  @fread($fp,filesize($uploadedFile));
-                        @fclose($fp);
-                        $data = chunk_split(base64_encode($data));
-                        $message .= "Content-Type: application/octet-stream; name=\"".basename($uploadedFile)."\"\n" . 
-                        "Content-Description: ".basename($uploadedFile)."\n" .
-                        "Content-Disposition: attachment;\n" . " filename=\"".basename($uploadedFile)."\"; size=".filesize($uploadedFile).";\n" . 
-                        "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
-                    }
-                    
-                    $message .= "--{$mime_boundary}--";
-                    $returnpath = "-f" . $email;
-                    
-                    // Send email
-                    $mail = mail($toEmail, $emailSubject, $message, $headers, $returnpath);
-                    
-                    // Delete attachment file from the server
-                    @unlink($uploadedFile);
-                }else{
-                     // Set content-type header for sending HTML email
-                    $headers .= "\r\n". "MIME-Version: 1.0";
-                    $headers .= "\r\n". "Content-type:text/html;charset=UTF-8";
-                    
-                    // Send email
-                    $mail = mail($toEmail, $emailSubject, $htmlContent, $headers); 
-                }
-                
-                // If mail sent
-                if($mail){
-                    $statusMsg = 'Your contact request has been submitted successfully !';
-                    $msgClass = 'succdiv';
-                    
-                    $postData = '';
-                }else{
-                    $statusMsg = 'Your contact request submission failed, please try again.';
-                }
-            }
-        }
-    }else{
-        $statusMsg = 'Please fill all the fields.';
-    }
-}*/
+$email = $_REQUEST['email'] ;
+$name = $_REQUEST['name'] ;
+$phone = $_REQUEST['contact'] ;
+$message = $_REQUEST['bio'] ;
+require("phpmailer/PHPMailerAutoload.php");
+
+$mail = new PHPMailer();
+
+$mail->IsSMTP();
+
+$mail->Host = "mail.marketingojo.com";
+
+$mail->SMTPAuth = true; 
+$mail->SMTPSecure = 'ssl'; 
+$mail->Username = "info@marketingojo.com"; // SMTP username
+$mail->Password = "Marketing@52"; // SMTP password
+$mail->addAttachment("uploads/".$file_name);
+$mail->From = $email;
+$mail->SMTPSecure = 'tls'; 
+$mail->Port = 465; //SMTP port
+$mail->addAddress("mohammedbori44@gmail.com", "Mohammed");
+$mail->Subject = "You have an email from a website visitor!";
+$mail->Body ="
+Name: $name<br>
+Email: $email<br>
+Telephone: $phone<br><br><br>
+Comments: $message";
+$mail->AltBody = $message;
+
+if(!$mail->Send())
+{
+echo "Message could not be sent. <p>";
+echo "Mailer Error: " . $mail->ErrorInfo;
+exit;
+}
+
+echo "<script>alert('Message has been sent')</script>";
 ?>
 
 <?php
-if(isset($_POST["submit"])){
+/*if(isset($_POST["submit"])){
 	require "PHPMailer/PHPMailerAutoload.php";
 function smtpmailer($to, $from, $from_name, $subject, $body)
     {
@@ -201,7 +142,7 @@ Website: - http://marketingojo.com/</p>";
   
 
     $error=smtpmailer($to,$from, $name ,$subj, $msg);
-}
+}*/
 ?>
 
 
