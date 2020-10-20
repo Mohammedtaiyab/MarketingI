0d01c1f1fed81075b720350fe8a6ebaca13173ad
mohammedbori <mohammedbori44@gmail.com>
2020-10-20 22:52:59 +0530

    New Admin

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -3,8 +3,9 @@
 <head>
   	<meta charset="utf-8">
   	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-  	<title>Ecommerce Site using PHP</title>
+  	<title>MarketingOJO</title>
   	<!-- Tell the browser to be responsive to screen width -->
+      <link rel="icon" type="image/png" href="../images/favicon.png">  
   	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   	<!-- Bootstrap 3.3.7 -->
   	<link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -2,9 +2,9 @@
   <!-- Logo -->
   <a href="#" class="logo">
     <!-- mini logo for sidebar mini 50x50 pixels -->
-    <span class="logo-mini"><b>C</b>P</span>
+    <span class="logo-mini"><b>OJO</b></span>
     <!-- logo for regular state and mobile devices -->
-    <span class="logo-lg"><b>Ecommerce</b>Site</span>
+    <span class="logo-lg"><b>OJO</b>Admin</span>
   </a>
   <!-- Header Navbar: style can be found in header.less -->
   <nav class="navbar navbar-static-top">
